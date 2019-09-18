d46ed8dc810c22327bdbaa40cd3fdff5414b8b44
Zeemah <jhodgson1@protonmail.ch>
2019-09-19 00:47:56 +0100

    some unfinished debugprint thingy

---
+++ team-base/shared/sh_utils.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+const DebugLog = (player = -1, message, severity = 0) => {
+    let prefix = IsDuplicityVersion() ? `[${GetCurrentResourceName()}]` : '[TEAM-BASE]';
+    switch (severity) {
+        case 0:
+			prefix += ' [INFO]';
+            break;
+
+        case 1:
+            prefix += ' [WARN]';
+            break;
+
+        case 2:
+            prefix += ' [ERROR]';
+            break;
+
+		default:
+            prefix += ' [INFO]';
+			break;
+	}
+};
